9ecd864ae131fea987be713301993a1a66cf2cbe
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-15 23:35:50 +0700

    Reinitialize SpringBoot Project

---
+++ src/test/java/jagongadpro/autentikasi/AutentikasiApplicationTests.java
@@ -0,0 +1,13 @@
+package jagongadpro.autentikasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentikasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
