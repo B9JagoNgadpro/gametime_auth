1b00c0c7efd3a574cd73f0825070e2ead1082fcf
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-22 22:05:25 +0700

    [RED] make test for JWTService

---
+++ src/test/java/jagongadpro/autentikasi/service/JwtServiceTest.java
@@ -0,0 +1,57 @@
+package jagongadpro.autentikasi.service;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import jagongadpro.autentikasi.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JwtServiceTest {
+    @Autowired
+    JwtService jwtService;
+    User userLogin = new User.Builder().email("abc@gmail.com").username("username").bio("bio").password("password").profileUrl("url").saldo(500000).build();
+    @Test
+    void extractUsernameTest() {
+        String token = jwtService.generateToken(userLogin);
+        assertEquals(jwtService.extractUsername(token),"abc@gmail.com" );
+    }
+
+    @Test
+    void testGenerateTokenTest() {
+        String token = jwtService.generateToken(userLogin);
+        assertNotNull(token);
+    }
+
+    @Test
+    void getExpirationTimeTest() {
+        assertEquals(3000L, jwtService.getExpirationTime());
+    }
+
+    @Test
+    void isTokenValidTestSuccess() {
+        String token = jwtService.generateToken(userLogin);
+        assertTrue(jwtService.isTokenValid(token,userLogin));
+    }
+
+    @Test
+    void isTokenValidTestNotSameUser() {
+        User other = new User.Builder().email("other@gmail.com").build();
+        String token = jwtService.generateToken(userLogin);
+        assertFalse(jwtService.isTokenValid(token,other));
+    }
+    @Test
+    void isTokenValidTestTimeExceed() throws InterruptedException {
+        String token = jwtService.generateToken(userLogin);
+        Thread.sleep(3000);
+        assertThrows(ExpiredJwtException.class, () -> jwtService.isTokenValid(token,userLogin));
+    }
+
+}
