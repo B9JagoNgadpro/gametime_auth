116b5e8e827dddde5a1d9bac6e19eec6cc6e07d8
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-23 21:40:11 +0700

    [RED] make test to validate null in login user req

--- src/test/java/jagongadpro/autentikasi/dto/LoginUserRequestTest.java
+++ src/test/java/jagongadpro/autentikasi/dto/LoginUserRequestTest.java
@@ -1,20 +1,50 @@
 package jagongadpro.autentikasi.dto;
 
+import jagongadpro.autentikasi.service.ValidationService;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class LoginUserRequestTest {
+    @Autowired
+    ValidationService validationService;
     @Test
     void LoginUserRequestSuccess(){
         LoginUserRequest user = new LoginUserRequest("abc@gmail.com", "password");
+        validationService.validate(user);
         assertEquals(user.getEmail(),"abc@gmail.com" );
         assertEquals(user.getPassword(), "password");
     }
 
     @Test
+    void LoginUserRequestEmailBlank(){
+        LoginUserRequest user = new LoginUserRequest("", "password");
+        assertThrows(ConstraintViolationException.class, ()->{
+            validationService.validate(user);
+        });
+    }
+    @Test
+    void LoginUserRequestPasswordBlank(){
+        LoginUserRequest user = new LoginUserRequest("abc@gmail.com", "");
+        assertThrows(ConstraintViolationException.class, ()->{
+            validationService.validate(user);
+        });
+    }
+    @Test
+    void LoginUserRequestPasswordNull(){
+        LoginUserRequest user = new LoginUserRequest("abc@gmail.com", null);
+        assertThrows(ConstraintViolationException.class, ()->{
+            validationService.validate(user);
+        });
+    }
+    @Test
     void LoginUserRequestEmailNull(){
         LoginUserRequest user = new LoginUserRequest(null, "password");
-
+        assertThrows(ConstraintViolationException.class, ()->{
+            validationService.validate(user);
+        });
     }
 }
