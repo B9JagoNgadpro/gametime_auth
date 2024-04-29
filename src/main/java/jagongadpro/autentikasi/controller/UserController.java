fc29ad31a906799a74f772b3a61963d958b1d91f
Naomi <gabriella.naomi21@ui.ac.id>
2024-04-29 09:07:54 +0700

    [REFACTOR] implement facade design pattern in user controller

--- src/main/java/jagongadpro/autentikasi/controller/UserController.java
+++ src/main/java/jagongadpro/autentikasi/controller/UserController.java
@@ -5,10 +5,7 @@ import jagongadpro.autentikasi.dto.WebResponse;
 import jagongadpro.autentikasi.model.PasswordResetToken;
 import jagongadpro.autentikasi.model.User;
 import jagongadpro.autentikasi.model.UserNotFoundException;
-import jagongadpro.autentikasi.service.EmailServiceImpl;
-import jagongadpro.autentikasi.service.PasswordResetTokenServiceImpl;
-import jagongadpro.autentikasi.service.UserService;
-import jagongadpro.autentikasi.service.ValidationService;
+import jagongadpro.autentikasi.service.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,63 +20,21 @@ import java.util.UUID;
 @RestController
 public class UserController {
     @Autowired
-    UserService userService;
-
-    @Autowired
-    ValidationService validationService;
-
-    @Autowired
-    EmailServiceImpl emailService;
-
-    @Autowired
-    PasswordResetTokenServiceImpl passwordResetTokenService;
+    UserFacade userFacade;
 
     @PostMapping(value = "/user/resetPassword", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WebResponse<String> resetPassword(HttpServletRequest request,
-                                             @RequestParam("email") String userEmail) {
-        User user = userService.findByEmail(userEmail);
-        if (user == null) {
-            throw new UserNotFoundException("Email tidak ditemukan");
-        }
-        String token = UUID.randomUUID().toString();
-        userService.createPasswordResetTokenForUser(user, token);
-        emailService.constructResetTokenEmail(getAppUrl(request), token, user);
-        return WebResponse.<String>builder().data("url sent").build();
+    public WebResponse<String> resetPassword(HttpServletRequest request,@RequestParam("email") String userEmail) {
+        return userFacade.resetPassword(request, userEmail);
     }
 
-    private String getAppUrl(HttpServletRequest request) {
-        String scheme = request.getScheme(); // http or https
-        String serverName = request.getServerName();
-        int serverPort = request.getServerPort();
-        String contextPath = request.getContextPath();
-        return scheme + "://" + serverName + ":" + serverPort + contextPath;
-    }
+
     @GetMapping("/user/changePassword")
     public ResponseEntity<String> showChangePasswordPage(@RequestParam("token") String token) {
-        String result = passwordResetTokenService.validatePasswordResetToken(token);
-        if(result.equals("invalid")) {
-            //nanti return redirect ke login page karna ga valid
-            return new ResponseEntity<>("invalid", HttpStatus.NOT_FOUND);
-            //valid nanti return ke halaman forget pw
-        } else {
-            return ResponseEntity.ok().body("valid");
-        }
+        return userFacade.showChangePasswordPage(token);
     }
     @PostMapping(value = "/user/savePassword", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse<String> savePassword(@RequestBody PasswordDto passwordDto) {
-        validationService.validate(passwordDto);
-        String result = passwordResetTokenService.validatePasswordResetToken(passwordDto.getToken());
-
-        if(result != null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "token tidak valid");
-        }
-        User user = passwordResetTokenService.getUserByPasswordResetToken(passwordDto.getToken());
-        if(user!=null) {
-            userService.changeUserPassword(user, passwordDto.getNewPassword());
-            return WebResponse.<String>builder().data("password berhasil diganti").build();
-        } else {
-            throw new UserNotFoundException("user tidak ditemukan");
-        }
+        return userFacade.savePassword(passwordDto);
     }
 
 }
