7aba3c0ddb62b42539f26cfd375723e486bb4e0e
Naomi <gabriella.naomi21@ui.ac.id>
2024-05-23 17:47:45 +0700

    add allowed origin

--- src/main/java/jagongadpro/autentikasi/AutentikasiApplication.java
+++ src/main/java/jagongadpro/autentikasi/AutentikasiApplication.java
@@ -1,26 +1,17 @@
 package jagongadpro.autentikasi;
 
-import jagongadpro.autentikasi.model.User;
-import jagongadpro.autentikasi.model.UserNotFoundException;
-import jagongadpro.autentikasi.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.function.ServerResponse;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.servlet.function.ServerResponse;
 
-import java.util.Collection;
+import org.springframework.web.servlet.function.RouterFunction;
+
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
@@ -28,6 +19,7 @@ import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFuncti
 
 @EnableConfigurationProperties
 @SpringBootApplication()
+@CrossOrigin(origins = "*")
 public class AutentikasiApplication {
 
 	public static void main(String[] args) {
@@ -49,5 +41,6 @@ public class AutentikasiApplication {
 	public RouterFunction<ServerResponse> getGameById() {
 		return route("/api/games/{id}").GET("/api/games/{id}", http("http://35.240.130.147/")).build();
 	}
+
 
 }
