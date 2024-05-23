07022de2cb872b611c50956261ca3e82384f404b
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-23 13:30:54 +0700

    [FIX] Refactor routing functions for clarity and error correction

--- src/main/java/jagongadpro/autentikasi/AutentikasiApplication.java
+++ src/main/java/jagongadpro/autentikasi/AutentikasiApplication.java
@@ -1,53 +1,45 @@
 package jagongadpro.autentikasi;
 
-import jagongadpro.autentikasi.model.User;
-import jagongadpro.autentikasi.model.UserNotFoundException;
-import jagongadpro.autentikasi.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.function.ServerResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import java.util.Collection;
-import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
 
-
-
 @EnableConfigurationProperties
-@SpringBootApplication()
+@SpringBootApplication
 public class AutentikasiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutentikasiApplication.class, args);
 	}
+
 	@Bean
 	public RouterFunction<ServerResponse> getAllGames() {
-		return route("/api/games/get-all").GET("/api/games/get-all", http("http://localhost:9090")).build();
+		return route().GET("/api/games/get-all", http("http://localhost:9090")).build();
 	}
+
 	@Bean
 	public RouterFunction<ServerResponse> createGames() {
-		return route("/api/games/create").POST("/api/games/create", http("http://localhost:9090")).build();
+		return route().POST("/api/games/create", http("http://localhost:9090")).build();
 	}
+
 	@Bean
 	public RouterFunction<ServerResponse> filterGames() {
-		return route("/api/games/filter").GET("/api/games/filter", http("http://34.87.89.120:8080/api/games/filter")).build();
+		return route().GET("/api/games/filter", http("http://34.87.89.120:8080/api/games/filter")).build();
 	}
+
 	@Bean
 	public RouterFunction<ServerResponse> getGameById() {
-		return route("/api/games/{id}").GET("/api/games/{id}", http("http://34.87.89.120:8080/api/games/{id}")).build();
+		return route().GET("/api/games/{id}", request -> {
+			String id = request.pathVariable("id");
+			String uri = "http://34.87.89.120:8080/api/games/" + id;
+			return http(uri).handle(request);
+		}).build();
 	}
 
 	@Bean
