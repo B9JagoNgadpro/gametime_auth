3ef3cec7b4ab3f8c309b5b67cbbbe7626d38d23a
Naomi <gabriella.naomi21@ui.ac.id>
2024-05-26 00:05:49 +0700

    [FIX] version od user controller test problem because merge conflict

--- src/test/java/jagongadpro/autentikasi/controller/UserControllerTest.java
+++ src/test/java/jagongadpro/autentikasi/controller/UserControllerTest.java
@@ -9,7 +9,6 @@ import jagongadpro.autentikasi.enums.Status;
 import jagongadpro.autentikasi.model.User;
 import jagongadpro.autentikasi.model.UserNotFoundException;
 
-import jagongadpro.autentikasi.service.PasswordResetTokenServiceImpl;
 import jagongadpro.autentikasi.service.UserFacade;
 import jagongadpro.autentikasi.service.UserService;
 
@@ -44,10 +43,9 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import org.springframework.security.test.context.support.WithMockUser;
-
 
 
 @SpringBootTest
@@ -66,8 +64,6 @@ public class UserControllerTest {
 
     @MockBean
     UserDetailsService userDetailsService;
-    @MockBean
-    PasswordResetTokenServiceImpl passwordResetTokenService;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -87,6 +83,7 @@ public class UserControllerTest {
                     });
                     assertEquals(response.getData(), "url sent" );
                 });
+
     }
     @Test
     public void testResetPasswordUserNotFound() throws Exception {
@@ -104,6 +101,7 @@ public class UserControllerTest {
                     assertNotNull(response.getErrors());
                     assertEquals(response.getErrors(),"Email tidak ditemukan");
                 });
+
     }
 
     @Test
@@ -117,18 +115,9 @@ public class UserControllerTest {
                     String response = result.getResponse().getContentAsString();
                     assertEquals(response, "valid");
                 } );
+
     }
 
-    @Test
-    public void testTokenInValid() throws  Exception{
-        when(passwordResetTokenService.validatePasswordResetToken("token")).thenReturn("invalid") ;
-        mockMvc.perform(get("/user/password/changePassword").param("token", "token"))
-                .andExpect(status().isNotFound())
-                .andDo(result ->{
-                    String response = result.getResponse().getContentAsString();
-                    assertEquals(response, "invalid");
-                } );
-    }
 
     @Test
     public void savePasswordSuccess() throws Exception{
@@ -138,51 +127,9 @@ public class UserControllerTest {
         when(userFacade.savePassword(any(PasswordDto.class))).thenReturn(responseMock) ;
         mockMvc.perform(post("/user/password/savePassword").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(passwordDto))).andExpect(status().isOk())
                 .andDo(result -> {
-                   WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
-                   });
-                   assertEquals(response.getData(), "password berhasil diganti");
-                });
-    }
-
-    @Test
-    public void savePasswordArgumentNotvalid() throws Exception{
-        User user = new User();
-        PasswordDto passwordDto = new PasswordDto("token","");
-        mockMvc.perform(post("/user/password/savePassword").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(passwordDto))).andExpect(status().isBadRequest())
-                .andDo(result -> {
-                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
                     });
-                    assertNull(response.getData());
-                    assertEquals(response.getErrors(), "newPassword: password tidak boleh kosong");
-                });
-    }
-
-    @Test
-    public void savePasswordTokenNotvalid() throws Exception{
-        User user = new User();
-        PasswordDto passwordDto = new PasswordDto("token","newPassword");
-        when(passwordResetTokenService.validatePasswordResetToken("token")).thenReturn("invalid");
-        mockMvc.perform(post("/user/password/savePassword").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(passwordDto))).andExpect(status().isNotFound())
-                .andDo(result -> {
-                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
-                    });
-                    assertNull(response.getData());
-                    assertEquals(response.getErrors(), "token tidak valid");
-                });
-    }
-
-    @Test
-    public void savePasswordUserNotFound() throws Exception{
-        User user = new User();
-        PasswordDto passwordDto = new PasswordDto("token","newPassword");
-        when(passwordResetTokenService.validatePasswordResetToken("token")).thenReturn(null);
-        when(passwordResetTokenService.getUserByPasswordResetToken("token")).thenReturn(null) ;
-        mockMvc.perform(post("/user/password/savePassword").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(passwordDto))).andExpect(status().isNotFound())
-                .andDo(result -> {
-                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
-                    });
-                    assertNull(response.getData());
-                    assertEquals(response.getErrors(), "user tidak ditemukan");
+                    assertEquals(response.getData(), "password berhasil diganti");
                 });
     }
 
@@ -198,14 +145,14 @@ public class UserControllerTest {
                     assertNull(response.getErrors());
                     assertEquals(response.getData().getEmail(),"testuser");
                     assertEquals(response.getData().getStatus(), Status.ROLE_PEMBELI);
+
                 });
     }
-
     @Test
     public void testGetUserNotLoggedIn() throws Exception{
         mockMvc.perform(get("/user/me").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is4xxClientError());
-    }
 
+    }
     @Test
     @WithMockUser(username = "testuser", authorities = {"ROLE_PEMBELI"})
     public void testUpdateUserBalance() throws Exception{
@@ -217,11 +164,12 @@ public class UserControllerTest {
                     assertEquals(response.getData(),"Ok");
                     assertNull(response.getErrors());
                 });
-    }
 
+    }
     @Test
     public void testSetUserBalanceNotLoggedIn() throws Exception{
         mockMvc.perform(patch("/user/reduceBalance").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is4xxClientError());
+
     }
 
     @Test
@@ -237,6 +185,9 @@ public class UserControllerTest {
                     assertEquals(response.getErrors(), "Saldo tidak valid");
                 });
     }
+
+
+
 
     @Test
     @WithMockUser(username = "user1@example.com", authorities = {"ROLE_PEMBELI"})
