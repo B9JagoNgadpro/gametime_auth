c2c49d085a37c8d84635f0cb8109a1910fe7e45e
suryata <madeputra415@gmail.com>
2024-05-26 23:45:21 +0700

    last push

--- src/main/java/jagongadpro/autentikasi/controller/AuthenticationController.java
+++ src/main/java/jagongadpro/autentikasi/controller/AuthenticationController.java
@@ -11,21 +11,36 @@ import jagongadpro.autentikasi.service.JwtService;
 import jagongadpro.autentikasi.service.ValidationService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
+
+@Configuration
+class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
 
 @RestController
 @RequestMapping("/api/auth/")
 public class AuthenticationController {
     @Autowired
     JwtService jwtService;
+
     @Autowired
     AuthenticationService authenticationService;
 
@@ -35,11 +50,14 @@ public class AuthenticationController {
     @Autowired
     EmailServiceImpl emailService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @PostMapping(value = "login", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse<LoginResponse> login(@RequestBody LoginUserRequest request){
-          //contraint violation exception
+        // Contraint violation exception
         validationService.validate(request);
-        //bisa ada eror bad doncern bla"gitu dah
+        // Bisa ada error bad doncern bla"gitu dah
         User authenticatedUser = authenticationService.authenticate(request);
         String jwtToken = jwtService.generateToken(authenticatedUser);
         LoginResponse loginResponse = new LoginResponse();
@@ -47,18 +65,21 @@ public class AuthenticationController {
         loginResponse.setExpiredIn(jwtService.getExpirationTime());
 
         return WebResponse.<LoginResponse>builder().data(loginResponse).build();
-
     }
-
 
     @PostMapping(value = "signup")
     public ResponseEntity<User> register(@RequestBody RegisterUserRequest registerUserDto) {
         User registeredUser = authenticationService.signup(registerUserDto);
 
-        //untuk membuat keranjang secara otomatis
-        // String email = registeredUser.getEmail();
-        // String apiUrl = "http://35.213.132.17/api/cart/create/" + email;
-        // restTemplate.postForEntity(apiUrl, null, String.class);
+        // Untuk membuat keranjang secara otomatis
+        String email = registeredUser.getEmail();
+        String apiUrl = "http://35.213.132.17/api/cart/create/" + email;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, entity, String.class);
 
         return ResponseEntity.ok(registeredUser);
     }
